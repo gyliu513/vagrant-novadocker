6c323a28ab368becf3083c4ab58eee98a47b4e42
luckyfengyong <fengyong.gm@gmail.com>
2015-04-20 22:37:35 -0400

    fix docker

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -8,22 +8,26 @@ function disableFirewall {
 
 function installUtilities {
     echo "install utilities"
-    apt-get update -y
-    apt-get install -y git git-review python-pip python-dev
-    # install docker
-    apt-get install -y docker.io
+
+	# install docker in ubuntu http://docs.docker.com/installation/ubuntulinux/
+	apt-get update -y
+	apt-get install -y curl git git-review python-pip python-dev
+	curl -sSL https://get.docker.com/ubuntu/ | sh
+
     # install nova-docker
     git clone https://github.com/qiujian16/nova-docker.git
     cd nova-docker
     python setup.py install
+
     # get devstack and create stack user
     cd /opt/
     git clone https://git.openstack.org/openstack-dev/devstack
     /opt/devstack/tools/create-stack-user.sh
     chown -R stack:stack devstack
+
     # start docker in stack group
-    echo "DOCKER_OPTS=\"-G stack\"" >> /etc/default/docker.io
-    service docker.io restart
+    echo "DOCKER_OPTS=\"-G stack\"" >> /etc/default/docker
+    service docker restart
 }
 echo "setup ubuntu"
 
